d17239f7f5f9988ad799f44e3a1fa87ea75c4d99
SrivatsVS <52534004+SrivatsVS@users.noreply.github.com>
2019-09-28 15:31:01 +0530

    Add files via upload

--- userlogin.php
+++ userlogin.php
@@ -3,11 +3,11 @@ $servername = "localhost:3306";
 $username = "root";
 $password = "billu1999";
 $dbname = "techathon";
-$user=$_POST['user'];
 
-echo "hello";
-$password=$_POST['password'];
-
+$user=$_POST['username'];
+$password1=$_POST['password'];
+echo $user;
+echo $password1;
 // Create connection
 $conn = new mysqli($servername, $username, $password,$dbname);
 
@@ -15,11 +15,11 @@ $conn = new mysqli($servername, $username, $password,$dbname);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } 
+$result = mysqli_query($conn,"SELECT * FROM user WHERE uid='" . $user . "' and password = '". $password1."'");
+$count  = mysqli_num_rows($result);
 
-$sql="select * from user where user='$user' and password='$password';";
-$res=mysqli_query($conn,$sql);
-$rows=mysqli_num_rows($res);
-if($rows>0)
+
+if($count>0)
 {
 	echo " welcome admin! ";
 }
